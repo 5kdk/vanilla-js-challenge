0921ffb6582c180c4457a189ea51418df09b8f1e
5kdk <tylerk1121@gmail.com>
2022-11-07 06:19:05 +0900

    🎨 불필요한 코드 정리

--- js/background.js
+++ js/background.js
@@ -1,15 +1,3 @@
-/* const image = ["0.png", "1.png", "2.png", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg"];
-
-const chosenImage = image[Math.floor(Math.random() * image.length)];
-
-const body = document.querySelector("body");
-const bgImage = document.createElement("img");
-bgImage.classList.add("bgimg");
-
-bgImage.src = `img/${chosenImage}`;
-
-document.body.appendChild(bgImage); */
-
 const bgColors = [
   "#55967e",
   "#a8dba8",
